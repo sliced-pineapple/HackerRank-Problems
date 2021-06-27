3ff51d574d822f02287a21ef885d90144341b432
sliced-pineapple <hasan.mahadi1542@gmail.com>
2021-06-27 23:03:51 +0600

    simple-array-sum

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int n; cin >> n;
+
+    int count  = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int temp; cin >> temp;
+        count += temp;
+    }
+    
+
+    cout << count << endl;
+}
